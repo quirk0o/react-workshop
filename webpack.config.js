c338f977dba7bcfc2de73566bd9d3f2212adfd4e
Quirk0o <beata@obrok.eu>
2016-07-21 16:53:14 +0200

    Add React Router as dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,51 @@
-
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  context: path.resolve('./app'),
-  entry: './index.js',
-  output: {
-    path: 'dist',
+  context:   path.resolve('./app'),
+  entry:     [
+    './index.js',
+    'bootstrap-loader'
+  ],
+  output:    {
+    path:     'dist',
     filename: 'bundle.js'
   },
-  devtool: 'eval-source-map',
+  devtool:   'source-map',
   devServer: {
     port: 3000
   },
-  module: {
+  module:    {
     loaders: [
       {
-        test: /\.jsx?$/,
+        test:    /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel',
-        query: {
+        loader:  'babel',
+        query:   {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test:    /\.css$/,
+        loaders: ['style', 'css', 'postcss']
+      },
+      {
+        test:    /\.scss$/,
+        loaders: ['style', 'css', 'sass', 'postcss']
+      },
+      {
+        test:   /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file'
       }
     ]
   },
-  resolve: {
+  resolve:   {
     extensions: ['', '.js', '.jsx', '.json']
   },
-  plugins: [new HtmlWebpackPlugin({
+  plugins:   [new HtmlWebpackPlugin({
     template: './index.html'
-  })]
+  })],
+  postcss:   [autoprefixer]
 };

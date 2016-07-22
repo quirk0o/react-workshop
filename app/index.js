dde329a286fe2b66585fc69534d3647f7365e9fa
Quirk0o <beata@obrok.eu>
2016-07-22 13:40:10 +0200

    Starting setup for creating a React app

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
+/*
+  This is the entry point to the app
+  Webpack will use this to create your bundle.js
+  That means whatever dependencies are imported here they will be resolved by Webpack and their dependencies and so on...
+  Import your main component and render it here
+ */
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import App from './components/App'
-
-ReactDOM.render(<App />, document.getElementById('app'));

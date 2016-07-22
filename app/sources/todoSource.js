24b77e31a88bef86437202a8282763f1ba22f172
Quirk0o <beata@obrok.eu>
2016-07-22 14:58:12 +0200

    Boilerplate React app and simple server

---
+++ app/sources/todoSource.js
@@ -0,0 +1,55 @@
+import $ from 'jquery'
+
+export default (url) => {
+  return {
+    fetch() {
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          url:      url + '/api/todos',
+          method:   'GET',
+          dataType: 'json',
+          success:  resolve,
+          error:    reject
+        });
+      });
+    },
+
+    create(todo) {
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          url:      url + '/api/todos',
+          method:   'POST',
+          dataType: 'json',
+          data:     todo,
+          success:  resolve,
+          error:    reject
+        });
+      });
+    },
+
+    update(todo) {
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          url:      url + '/api/todos/' + todo.id,
+          method:   'PUT',
+          dataType: 'json',
+          data:     todo,
+          success:  resolve,
+          error:    reject
+        });
+      });
+    },
+
+    remove(id) {
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          url:      url + '/api/todos/' + id,
+          method:   'DELETE',
+          dataType: 'json',
+          success:  resolve,
+          error:    reject
+        });
+      });
+    }
+  };
+}
